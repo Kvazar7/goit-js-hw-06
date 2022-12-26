15c1f456429ee411a1a41bdb641602128885d562
Kvazar7 <Kometa737@gmail.com>
2022-12-26 03:19:02 +0200

    1

--- js/task-05.js
+++
@@ -1,6 +0,0 @@
-const inputText = document.querySelector("#name-input");
-const output = document.querySelector("#name-output");
-
-inputText.addEventListener("input", (event) => {
-  output.textContent = event.currentTarget.value;
-});
--- js/task-07.js
+++
@@ -1,9 +0,0 @@
-const inputEl = document.querySelector('#font-size-control');
-const outputEl = document.querySelector('#text');
-
-inputEl.addEventListener('input', checkInput);
-
-function checkInput(event, changeTextSize) {
-    outputEl.style.fontSize = this.value + 'px';
-    console.log(this.value)
-};
